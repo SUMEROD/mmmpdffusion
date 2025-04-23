77c9cf734b279adfba27e8f71855c6ba4b184b15
SUMEROD <149680624+SUMEROD@users.noreply.github.com>
2025-04-23 23:30:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ def merge_pdfs():
             pass
 
 if __name__ == '__main__':
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
